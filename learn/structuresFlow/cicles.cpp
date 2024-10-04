f58c04b877976ddb6604a37424cb96cd267a9f3c
kalet Chavez <kalet.svdev@gmail.com>
2024-10-03 23:47:11 -0600

    :sparkles: add cicles.cpp file

---
+++ learn/structuresFlow/cicles.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[])
+{
+    string password, email;
+    bool validData = false;
+
+    //ciclo while
+    int counter = 0;
+    while( 6 > counter ){
+        cout << "6 es mayor que:" << counter << "\n";
+        counter = counter + 1;
+    }
+
+    char letra;
+
+    while( letra != 'x' ){
+        cout << "Ingrese una letra" << endl;
+        cin >> letra;
+    }
+
+    //bucle mas complejo
+    cout << "Ingrese un password" << "\n";
+    cin >> password;
+
+    cout << "Ingrese un email" << "\n";
+    cin >> email;
+
+    while( sizeof(password) > 0 && sizeof(email) > 0 && validData == false ){
+        if( email.find("@") != string::npos && email.find(".com") != string::npos ){
+            cout << "Bienvenido\n";
+            validData = true;
+        }else{
+            cout << "Ingrese un email correcto" << "\n";
+            cin >> email;
+        }
+    }
+
+   //lo mismo pero con un ciclo for
+
+   for( int counter = 0; 6 > counter; counter++ ){
+      cout << "6 es mayor que:" << counter << "\n";
+    }
+
+    //segundo bucle
+
+    for( char letra; letra != 'x'; cin >> letra ){
+        cout << "Ingrese una letra" << endl;
+    }
+
+    //for como bucle mas complejo
+
+    //borramos sus valores
+
+    for( bool validData = false; validData == false;  ){
+        string password, email;
+
+        cout << "Ingrese un password" << "\n";
+        cin >> password;
+
+        cout << "Ingrese un email" << "\n";
+        cin >> email;
+
+        if( email.find("@") != string::npos && email.find(".com") != string::npos ){
+            
+            cout << "Bienvenido\n";
+            validData = true;
+
+        }else{
+            cout << "Ingrese un email correcto" << "\n";
+            cin >> email;
+        }
+    }
+
+
+    return 0;
+}
